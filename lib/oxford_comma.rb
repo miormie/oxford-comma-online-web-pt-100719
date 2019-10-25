213ca99088ec4ea61b89e6282593908196c63be1
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-25 03:29:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ elsif array.length == 2
 array.join(" and ")
 
 elsif array.length == 3
-new_array = array.join(", ") && array.join(" and ")
+new_array = array.join(", ") || array.join(" and ")
 
 
 
