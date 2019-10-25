908a9d3be0f0950b3144549345959be9629c0d4c
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-25 03:55:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,7 @@ elsif array.length == 2
 array.join(" and ")
 
 elsif array.length >= 3
-#new_array =
-arrayarray[-1].prepend " and "
+new_array = arrayarray[-1].prepend " and "
 #array.join(", ")
 
 end 
