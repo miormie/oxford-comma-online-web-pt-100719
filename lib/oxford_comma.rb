1be87822662e5ca511a0c1373be98207bf44ca7e
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-25 03:48:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ elsif array.length == 2
 array.join(" and ")
 
 elsif array.length >= 3
-array[-1].prepend(" and ")
+new_array = arrayarray[-1].prepend(" and ")
 
 #new_arrayarray[-1].prepend(-1, 'and '
 
